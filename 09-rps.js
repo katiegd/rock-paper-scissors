7e05d458cafb21984d6710a935776c1c492be1ee
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-02-02 15:32:58 -0500

    Add actual files with button game.

---
+++ 09-rps.js
@@ -0,0 +1,75 @@
+let score =  JSON.parse(localStorage.getItem('score')) || //THIS IS A DEFAULT OPERATOR
+{
+    wins: 0,
+    losses: 0,
+    ties: 0
+
+}
+
+updateScoreElement();
+
+
+function getComputerMove() {
+    let computerChoice = '';
+    let randNum = Math.floor(Math.random() * 3); {
+    if(randNum === 0) {
+       computerChoice = 'rock';
+         } else if(randNum === 1) {
+       computerChoice = 'paper';
+         } else {
+            computerChoice = 'scissors';
+        }
+     }
+     return computerChoice;
+    }
+
+function playGame(playerMove) {
+    let computerChoice = getComputerMove();
+    let result = '';
+    if(playerMove === computerChoice) {
+        result = 'It is a tie.'
+    } else if((playerMove === 'rock' && computerChoice === 'paper') || 
+    (playerMove === 'scissors' && computerChoice === 'rock') || 
+    (playerMove === 'paper' && computerChoice === 'scissors')) { 
+        result = 'You lose!'
+    } else {
+        result = 'You win!'
+    }
+
+    if(result === 'You win!') {
+        score.wins += 1;
+    } else if(result === 'You lose!') {
+        score.losses += 1;
+    } else if(result === 'It is a tie.')
+        score.ties += 1;
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement();
+
+    document.querySelector('.js-result')
+        .textContent = result;
+
+    document.querySelector('.js-user-comp-moves')
+    .textContent = `You chose ${playerMove}. Computer chose ${computerChoice}.`;
+
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score')
+    .textContent = `Wins: ${score.wins} // Losses: ${score.losses} // Ties: ${score.ties}`;
+
+    if(score.wins === 5) {
+        document.querySelector('.js-score').textContent = 'Congratulations! You won 5 rounds.'
+    } else if(score.losses === 5) {
+        document.querySelector('.js-score').textContent = 'Sorry, you lost to the almighty computer.';
+    }
+}   
+
+function resetScore() {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+}
